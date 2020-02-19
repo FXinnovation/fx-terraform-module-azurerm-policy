6d21867ae36c031ed946fe4914c32d4079387776
ravi <ravi.periketi@fxinnovation.com>
2020-02-19 16:49:03 -0500

    remove the custom assignmnet parameter example

--- examples/custom_policy/deploy.tf
+++ examples/custom_policy/deploy.tf
@@ -22,16 +22,16 @@ module "custom" {
   source = "../.."
 
   policy_definition_enabled = true
-  policy_names              = ["tftest${random_string.this.result}", "foo${random_string.this.result}"]
+  policy_names              = ["tftest${random_string.this.result}"]
   policy_types              = ["Custom"]
   policy_modes              = ["All"]
-  policy_display_names      = ["terraform_test${random_string.this.result}", "terraform_test${random_string.this.result}"]
+  policy_display_names      = ["terraform_test${random_string.this.result}"]
 
   path_to_policy_definition_rules      = "${local.custom_policy_rules_files_string}"
   path_to_policy_definition_metadatas  = "${local.custom_policy_metadata_files_string}"
   path_to_policy_definition_parameters = "${local.custom_policy_parameters_files_string}"
 
   policy_assignment_enabled = true
-  policy_assignment_names   = ["tftest${random_string.this.result}", "foo${random_string.this.result}"]
+  policy_assignment_names   = ["tftest${random_string.this.result}"]
   policy_assignment_scopes  = ["/subscriptions/${var.subscription_id}"]
 }
