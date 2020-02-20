4d2cd56eb261e2e4a26cbc2bb32a081971d34566
ravi <ravi.periketi@fxinnovation.com>
2020-02-20 09:30:56 -0500

    enable policy assignment in example

--- examples/existing_policy/deploy.tf
+++ examples/existing_policy/deploy.tf
@@ -14,6 +14,7 @@ resource "azurerm_resource_group" "example" {
 module "existing" {
   source = "../.."
 
+  policy_assignment_enabled               = true
   policy_assignment_names                 = ["tf${random_string.this.result}"]
   policy_assignment_scopes                = ["/subscriptions/${var.subscription_id}"]
   policy_assignment_policy_definition_ids = ["/providers/Microsoft.Authorization/policyDefinitions/0e60b895-3786-45da-8377-9c6b4b6ac5f9"]
