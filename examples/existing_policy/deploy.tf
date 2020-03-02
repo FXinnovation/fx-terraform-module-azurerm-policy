b0c457a2e1fad9ef73edda0b2c44faa4dbb92cfa
ravi <ravi.periketi@fxinnovation.com>
2020-03-01 20:45:07 -0500

    fix example

--- examples/existing_policy/deploy.tf
+++ examples/existing_policy/deploy.tf
@@ -14,10 +14,10 @@ resource "azurerm_resource_group" "example" {
 module "existing" {
   source = "../.."
 
-  policy_assignment_enabled               = true
-  policy_assignment_names                 = ["tf${random_string.this.result}"]
-  policy_assignment_scopes                = ["/subscriptions/${var.subscription_id}"]
-  policy_assignment_policy_definition_ids = ["/providers/Microsoft.Authorization/policyDefinitions/0e60b895-3786-45da-8377-9c6b4b6ac5f9"]
-  policy_assignment_locations             = ["westeurope"]
-  policy_assignment_display_names         = ["boo${random_string.this.result}"]
+  policy_assignment_enabled       = true
+  policy_assignment_names         = ["tf${random_string.this.result}"]
+  policy_assignment_scopes        = ["/subscriptions/${var.subscription_id}"]
+  existing_policy_definition_ids  = ["/providers/Microsoft.Authorization/policyDefinitions/0e60b895-3786-45da-8377-9c6b4b6ac5f9"]
+  policy_assignment_locations     = ["westeurope"]
+  policy_assignment_display_names = ["boo${random_string.this.result}"]
 }
